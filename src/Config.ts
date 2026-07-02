f22ffb6b28fca816d121a8a320a52d0178149e75
SilverAlex78 <a.pizzillo@tiscali.it>
2026-07-03 00:20:23 +0200

    Add GameConfig for Phaser game setup

---
+++ src/Config.ts
@@ -0,0 +1,45 @@
+import Phaser from "phaser";
+
+import BootScene from "./scenes/BootScene";
+
+export const GameConfig: Phaser.Types.Core.GameConfig = {
+
+    type: Phaser.AUTO,
+
+    parent: "game",
+
+    width: 1280,
+
+    height: 720,
+
+    backgroundColor: "#071420",
+
+    pixelArt: false,
+
+    physics: {
+
+        default: "arcade",
+
+        arcade: {
+
+            debug: false
+
+        }
+
+    },
+
+    scale: {
+
+        mode: Phaser.Scale.FIT,
+
+        autoCenter: Phaser.Scale.CENTER_BOTH
+
+    },
+
+    scene: [
+
+        BootScene
+
+    ]
+
+};
